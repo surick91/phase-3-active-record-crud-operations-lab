2e0fe437b1227d5a0f7d98c03fc9329b9b3afda8
sam <surickj@gmail.com>
2021-08-22 13:21:07 -0700

    commit tree dd14a78ffaaaf784528139729de62c56712b3268

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ActiveRecord::Base
-
+    def can_be_instantiated_and_saved
+        movie = Movie.new
+        movie.title = "This is a title"
+        movie.save
+    end
+    def can_return_first_item_in_the_table
+        Movie.first
+       
+    end
 end
